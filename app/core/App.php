eb3c9a90c874f6595f103e28a3b8cfcfedd29c75
superunison777 <worthreg@gmail.com>
2017-01-13 10:06:21 -0500

    added method calling

--- app/core/App.php
+++ app/core/App.php
@@ -5,11 +5,10 @@ class App
 
 	protected $controller = "home";
 	protected $method = "index";
-	protected $params = Array();
+	protected $params = Array(); // maybe want new stdClass() instead, per https://stackoverflow.com/questions/1434368/how-to-define-an-empty-object-in-php
 
 	public function __construct() {
 		$url = $this->parseUrl();
-		print_r($url);
 
 		if (file_exists("../app/controllers/{$url[0]}.php")) {
 			$this->controller = $url[0];
@@ -18,11 +17,18 @@ class App
 
 		require_once("../app/controllers/{$this->controller}.php");
 		$this->controller = new $this->controller;
+
 		if (isset($url[1])) {
 			if (method_exists($this->controller, $url[1])) {
-				echo "OK";
+				$this->method = $url[1];
+				unset($url[1]);
 			}
 		}
+
+		$this->params = $url ? array_values($url) : []; // again with the dubious []
+
+		$call_user_func_array([$this->controller, $this->method], $this->params);
+
 	}
 
 	public function parseUrl() {
